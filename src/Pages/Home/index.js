f88cdd5dae41d8bb7bbc8fd1c5ae8e2d7b95ab7c
Frederic Zai <frederic.zai@gmail.com>
2024-05-20 05:43:33 +0200

    change screen

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,7 +3,7 @@ import Banner from "../../Componnent/Header/banner/banner";
 import Skills from "../../Componnent/Sections/Skills/skills";
 import Project from "../../Componnent/Sections/Project/project";
 import { Link, useParams } from 'react-router-dom';
-import cvFred from '../../images/CV_ZaiFr_AlternanceStage.pdf';
+import cvFred from '../../images/Cv_FZaï_versionFr.pdf';
 import "./index.scss";
 // Redux import 
 
