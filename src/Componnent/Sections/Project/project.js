ae7be3a243249bb814ad20a00dc90a5435531467
Frederic Zai <frederic.zai@gmail.com>
2024-12-16 11:06:54 +0100

    last 2.0

--- src/Componnent/Sections/Project/project.js
+++ src/Componnent/Sections/Project/project.js
@@ -16,7 +16,7 @@ import jardins from  "../../../images/1001Jardins.png";
 import Appnal from "../../../images/Appnal.png";
 import Elezov from "../../../images/Elezov.png";
 import Annecy from "../../../images/Annecy.jpeg";
-import Plan from "../../../images/plan.jpeg";
+import Plan from "../../../images/planO.png";
 
 // Redux import
 // Hooks
